29f2c3e4f1578ab899ab9f03ca884b92c3418546
Himanshu <him.raw.official1@gmail.com>
2022-12-18 00:09:35 +0530

    navlink on login installed

--- src/components/HomePageComponents/Navbar.jsx
+++ src/components/HomePageComponents/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Button } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
 import React from 'react'
-
+import { Navigate, NavLink } from 'react-router-dom';
 function Navbar() {
     return (
         <>
@@ -15,7 +15,9 @@ function Navbar() {
                     <Button sx={{padding:"6px 15px",fontWeight:"4px",letterSpacing:"1px",color:"white"}} variant="outline" >Search User</Button>
                     </a>
                     <a>
+                    <NavLink to={"/login"}>
                     <Button sx={{padding:"6px 15px"}} color="primary" variant='contained' endIcon={<LoginIcon/>}>Login</Button>
+                    </NavLink>
                     </a>
                 </div>
             </div>
