6716ffe9181751f6ead8b6a535e7502ec4097036
Himanshu <him.raw.official1@gmail.com>
2022-12-19 18:32:22 +0530

    added more functionality to loginpage

--- src/components/LoginPageComponents/LoginForm.jsx
+++ src/components/LoginPageComponents/LoginForm.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { Button, TextField } from '@mui/material'
-function LoginForm() {
+import { useState } from 'react'
+function LoginForm(props) {
+  const [userData,setUserData]=useState({
+    username:"",
+    password:""
+  });
+
+  function handelChange(event){
+    const {name,value}=event.target;
+    setUserData(prevVal=>({...userData,[name]:value}));
+  }
+  
+  function handelSubmit(){
+    console.log(userData);
+  }
+
+  function handelNewUser(){
+    props.setNewUser(true);
+  }
   return (
     <div className='login__form'>
-      <TextField className='login-form-inputs' id="outlined-basic" label="Username" variant="outlined" margin='normal' fullWidth sx={{backgroundColor:"rgba(239, 239, 239, 0.461)"}} />
-      <TextField className='login-form-inputs' id="outlined-basic" label="Password" type={"password"} variant="outlined" margin='normal' fullWidth sx={{backgroundColor:"rgba(239, 239, 239, 0.461)"}} />
-      <Button variant='contained' color='success' sx={{marginTop:"5%"}} fullWidth>login</Button>
-      <p className='signup__button'>not registered with us? <Button>SignUp Now</Button> </p>
+      <TextField onChange={handelChange}  name="username" className='login-form-inputs' label="Username" variant="outlined" margin='normal' fullWidth sx={{backgroundColor:"rgba(239, 239, 239, 0.461)"}} />
+      <TextField  onChange={handelChange} name="password" className='login-form-inputs' label="Password" type={"password"} variant="outlined" margin='normal' fullWidth sx={{backgroundColor:"rgba(239, 239, 239, 0.461)"}} />
+      <Button onClick={handelSubmit} variant='contained' color='success' sx={{marginTop:"5%"}} fullWidth>login</Button>
+      <p className='signup__button'>not registered with us? <Button onClick={handelNewUser} >SignUp Now</Button> </p>
     </div>
   )
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from '../components/HomePageComponents/Footer'
 import Navbar from '../components/HomePageComponents/Navbar'
 import LoginForm from '../components/LoginPageComponents/LoginForm'
+import SignUpForm from '../components/LoginPageComponents/SignUpForm';
 
 function LoginPage() {
+  const [newUser,setNewUser] = useState(false) ;
   return (
     <>
       <div className="login__body">
       <Navbar/>
-      <LoginForm/>
+      {!newUser?<LoginForm setNewUser={setNewUser} />:<SignUpForm/>}
       <Footer/>
       </div>
     </>
