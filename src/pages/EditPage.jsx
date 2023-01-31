d6e93ff13db05fbbb90d0cf272e6c9599d9cc34a
Himanshu <him.raw.official1@gmail.com>
2023-01-31 17:45:54 +0530

    add navbar and footer in edit page

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import DashboardNavbar from "../components/DashboardComponents/DashboardNavbar";
 import EditForm from "../components/EditPageComponents/EditForm";
+import Footer from "../components/HomePageComponents/Footer";
 
 function EditPage() {
   const { id } = useParams();
   return (
     <>
+    <DashboardNavbar/>
     <EditForm id={id}/>
+    <Footer/>
     </>
   );
 }
