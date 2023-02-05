0ac33147590b6143bd9d1f375312a2ee6dd9e147
Himanshu <him.raw.official1@gmail.com>
2023-02-05 10:17:25 +0530

    make newCode page private

--- src/pages/NewCode.jsx
+++ src/pages/NewCode.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React , {useEffect} from 'react'
 import DashboardNavbar from '../components/DashboardComponents/DashboardNavbar'
 import NewCodeForm from '../components/EditPageComponents/NewCodeForm'
 import Footer from '../components/HomePageComponents/Footer'
-
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 function NewCode() {
+  const navigate = useNavigate();
+  const {isLogin} = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (!isLogin) {
+      navigate("/login");
+    }
+  }, [isLogin]);
   return (
     <>
         <DashboardNavbar/>
