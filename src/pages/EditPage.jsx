684233cb0830ee52f205fb8f0542b2130a478abf
Himanshu <him.raw.official1@gmail.com>
2023-02-09 15:43:41 +0530

    making editPage private

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React , {useEffect} from "react";
 import { useParams } from "react-router-dom";
 import DashboardNavbar from "../components/DashboardComponents/DashboardNavbar";
 import EditForm from "../components/EditPageComponents/EditForm";
 import Footer from "../components/HomePageComponents/Footer";
 import Loader from "../components/DashboardComponents/Loader"
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Redirecting from "../components/LoginPageComponents/Redirecting";
 
 function EditPage() {
+  const navigate = useNavigate();
   const { isLoading } = useSelector((store) => store.editCode);
+  const {isLogin} = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (!isLogin) {
+      navigate("/login");
+    }
+  }, [isLogin]);
   const { id } = useParams();
   return (
     <>
+    {isLogin?<>
       {isLoading && <Loader/>}
       <DashboardNavbar />
       <EditForm id={id} />
       <Footer />
+    </>:<Redirecting/>}
     </>
   );
 }
