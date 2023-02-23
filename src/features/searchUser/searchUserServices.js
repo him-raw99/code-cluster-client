95a731f8b00cb3d1e3967bc3f38ff40936d7b8d9
Himanshu <him.raw.official1@gmail.com>
2023-02-23 20:56:09 +0530

    getting codes in the store

--- src/features/searchUser/searchUserServices.js
+++ src/features/searchUser/searchUserServices.js
@@ -4,7 +4,7 @@ const url = "https://code-cluster-api.onrender.com";
 
 //search user
 const searchUser = async (state) => {
-    const response = await axios.post(url+"/codes",state.user);
+    const response = await axios.get(url+"/user/"+state.user);
     return response;
 }
 
--- src/features/searchUser/searchUserSlice.jsx
+++ src/features/searchUser/searchUserSlice.jsx
@@ -2,16 +2,16 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import searchUserServices from "./searchUserServices";
 
 const initialState = {
-    user:[],
     isLoading:false,
     success:false,
     error:"",
+    codes:[],
 }
 
 export const searchUser = createAsyncThunk("searchUserSlice/searchUser",async(state)=>{
     try {
         const res = await searchUserServices.searchUser(state);
-        console.log(res);
+        return res;
     } catch (error) {
         console.log(error);
     }
@@ -21,7 +21,20 @@ const searchUserSlice = createSlice({
     name:"searchUser",
     initialState,
     reducers:{},
-    extraReducers:{},
+    extraReducers:{
+        [searchUser.pending]:(state)=>{
+            state.isLoading=true;
+        },
+        [searchUser.fulfilled]:(state,action)=>{
+            state.isLoading=false;
+            state.codes = action.payload.data.codes;
+            state.success = action.payload.data.success;
+        },
+        [searchUser.rejected]:(state,action)=>{
+            state.isLoading=false;
+            state.error = action.payload.error;
+        },
+    },
 })
 
 export default searchUserSlice.reducer; 
