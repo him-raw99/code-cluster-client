202fb7d7a98aecbac30d687054cd47c784940f24
Himanshu Rawat <97152850+him-raw99@users.noreply.github.com>
2023-04-06 22:33:57 +0530

    add error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from '../components/HomePageComponents/Navbar'
+import Footer from '../components/HomePageComponents/Footer'
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <>
+      <div style={{backgroundColor:"black",height:"100%"}}>
+      <img src='https://e1.pxfuel.com/desktop-wallpaper/595/681/desktop-wallpaper-are-you-lost-little-one-neon-fnaf-thumbnail.jpg' className='center-image'
+       />
+      <Navbar/>
+      <Footer/>
+      </div>
+    </>
   )
 }
 
