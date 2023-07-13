6c828f66207453ee8e501279935f136c09479fce
Himanshu Rawat <97152850+him-raw99@users.noreply.github.com>
2023-07-13 17:43:48 +0530

    create base to add syntax highlighting

--- src/components/EditPageComponents/EditForm.jsx
+++ src/components/EditPageComponents/EditForm.jsx
@@ -6,145 +6,182 @@ import Switch from "@mui/material/Switch";
 import CloseIcon from "@mui/icons-material/Close";
 import SaveAsIcon from "@mui/icons-material/SaveAs";
 import Button from "@mui/material/Button";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 import { useNavigate } from "react-router-dom";
+import SyntaxHighlighter from "react-syntax-highlighter";
 import {
-  close,
-  deleteModal,
-  getFullCode,
-  updateCode,
+    close,
+    deleteModal,
+    getFullCode,
+    updateCode,
 } from "../../features/editCode/editCodeSlice";
 import "./EditForm.css";
 
 function EditForm(props) {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { token } = useSelector((store) => store.auth);
-  const { isLoading, codeState, backToDashboard } = useSelector(
-    (store) => store.editCode
-  );
-  const [edittedCode, setEdittedState] = useState({
-    title: "",
-    code: "",
-    isPublic: false,
-  });
-  useEffect(() => {
-    dispatch(getFullCode({ token, id: props.id }));
-  }, []);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { token } = useSelector((store) => store.auth);
+    const { isLoading, codeState, backToDashboard } = useSelector(
+        (store) => store.editCode
+    );
+    const [edittedCode, setEdittedState] = useState({
+        title: "",
+        code: "",
+        isPublic: false,
+    });
+    useEffect(() => {
+        dispatch(getFullCode({ token, id: props.id }));
+    }, []);
 
-  useEffect(() => {
-    setEdittedState(codeState);
-    {
-      backToDashboard && navigate("/dashboard");
+    useEffect(() => {
+        setEdittedState(codeState);
+        {
+            backToDashboard && navigate("/dashboard");
+        }
+    }, [isLoading]);
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setEdittedState((prevValue) => {
+            return { ...prevValue, [name]: value };
+        });
     }
-  }, [isLoading]);
 
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setEdittedState((prevValue) => {
-      return { ...prevValue, [name]: value };
-    });
-  }
+    function makePublic() {
+        setEdittedState((prevVal) => {
+            return { ...prevVal, isPublic: !edittedCode.isPublic };
+        });
+    }
 
-  function makePublic() {
-    setEdittedState((prevVal) => {
-      return { ...prevVal, isPublic: !edittedCode.isPublic };
-    });
-  }
+    const codeString = `
+#include <iostream>
+using namespace std;
 
-  return (
-    <>
-      {!isLoading && (
+int fact(int n){
+    if (n==1)return 1;
+    return n*fact(n-1);
+}
+
+int main() {
+    // Write C++ code here
+    cout << fact(5);
+    
+    //5! = 5X4X3X2X1
+
+    return 0;
+}
+
+// ABC
+/*
+1>ABC
+2>ACB
+3>CAB
+4>CBA
+5>BAC
+6>BCA
+
+*/
+    `;
+
+    return (
         <>
-          <div className="form-container">
-            <h1 className="newCodeForm-title">Edit Code</h1>
-            <TextField
-              id="outlined-basic"
-              margin="normal"
-              placeholder="Add a title"
-              label="Title"
-              value={edittedCode.title || ""}
-              onChange={handleChange}
-              variant="outlined"
-              required
-              name="title"
-              fullWidth
-            />
-            <br />
-            <TextField
-              id="outlined-textarea"
-              label="Code"
-              placeholder="Write Code"
-              multiline
-              name="code"
-              margin="normal"
-              className="newLine"
-              value={edittedCode.code || ""}
-              onChange={handleChange}
-              minRows={4}
-              required
-              fullWidth
-            />
-            <br />
-            <br />
-            <FormControlLabel
-              labelPlacement="start"
-              control={
-                <Switch
-                  checked={edittedCode.isPublic || false}
-                  onChange={() => {
-                    makePublic();
-                  }}
-                />
-              }
-              label="Public"
-              className="toggleButton"
-              sx={{ marginLeft: "0" }}
-            />
+            {!isLoading && (
+                <>
+                    <div className="form-container">
+                        <h1 className="newCodeForm-title">Edit Code</h1>
+                        <TextField
+                            id="outlined-basic"
+                            margin="normal"
+                            placeholder="Add a title"
+                            label="Title"
+                            value={edittedCode.title || ""}
+                            onChange={handleChange}
+                            variant="outlined"
+                            required
+                            name="title"
+                            fullWidth
+                        />
+                        <br />
+                        <TextField
+                            id="outlined-textarea"
+                            label="Code"
+                            placeholder="Write Code"
+                            multiline
+                            name="code"
+                            margin="normal"
+                            className="newLine"
+                            value={edittedCode.code || ""}
+                            onChange={handleChange}
+                            minRows={4}
+                            required
+                            fullWidth
+                        />
+                        <br />
+                        <br />
+                        <FormControlLabel
+                            labelPlacement="start"
+                            control={
+                                <Switch
+                                    checked={edittedCode.isPublic || false}
+                                    onChange={() => {
+                                        makePublic();
+                                    }}
+                                />
+                            }
+                            label="Public"
+                            className="toggleButton"
+                            sx={{ marginLeft: "0" }}
+                        />
 
-            <div className="button-holder-newCode button-holder-editCode">
-              <Button
-                variant="contained"
-                color="success"
-                endIcon={<SaveAsIcon />}
-                onClick={() => {
-                  dispatch(updateCode({ token, id: props.id, edittedCode }));
-                }}
-                sx={{ marginRight: "1rem" }}
-                size="large"
-              >
-                Save
-              </Button>
+                        <div className="button-holder-newCode button-holder-editCode">
+                            <Button
+                                variant="contained"
+                                color="success"
+                                endIcon={<SaveAsIcon />}
+                                onClick={() => {
+                                    dispatch(
+                                        updateCode({
+                                            token,
+                                            id: props.id,
+                                            edittedCode,
+                                        })
+                                    );
+                                }}
+                                sx={{ marginRight: "1rem" }}
+                                size="large"
+                            >
+                                Save
+                            </Button>
 
-              <Button
-                variant="contained"
-                endIcon={<DeleteIcon />}
-                onClick={() => {
-                dispatch(deleteModal());
-              }}
-              sx={{ marginRight: "1rem" }}
-                color="error"
-                size="large"
-              >
-                Delete
-              </Button>
-              <Button
-                variant="outlined"
-                endIcon={<CloseIcon />}
-                onClick={() => {
-                  dispatch(close());
-                }}
-                color="warning"
-                size="large"
-              >
-                Close
-              </Button>
-            </div>
-          </div>
+                            <Button
+                                variant="contained"
+                                endIcon={<DeleteIcon />}
+                                onClick={() => {
+                                    dispatch(deleteModal());
+                                }}
+                                sx={{ marginRight: "1rem" }}
+                                color="error"
+                                size="large"
+                            >
+                                Delete
+                            </Button>
+                            <Button
+                                variant="outlined"
+                                endIcon={<CloseIcon />}
+                                onClick={() => {
+                                    dispatch(close());
+                                }}
+                                color="warning"
+                                size="large"
+                            >
+                                Close
+                            </Button>
+                        </div>
+                    </div>
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 }
 
 export default EditForm;
